eeb9687b9d6d7e9f7a63b12596ae7f0f6089ba7e
josiah <josiahdamiwilliams@gmail.com>
2020-03-06 09:20:35 +0100

    action types

--- africa/src/state/actionTypes.js
+++ africa/src/state/actionTypes.js
@@ -1,12 +1,13 @@
 //step 2 :Action Types
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const REGISTER_SUCCESS ="REGISTER_SUCCESS";
 export const LOG_OUT = "LOG_OUT";
-export const SUBMITTED_USER_DETAILS = "SUBMITTED_USER_DETAILS";
-export const DELETED_USER_DETAILS = "DELETED_USER_DETAILS";
 export const GET_ITEMS = "GET_ITEMS"
 export const CREATE_ITEMS = "CREATE_ITEMS ";
-export const UPDATE_ITEMS = "UPDATE_ITEMS";
-export const DELETE_ITEMS = "DELETE_ITEMS";
+export const READ_ITEMS ="READ_ITEMS"
+export const UPDATE_ITEMS = "UPDATE_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const EDIT_ITEM = "EDIT_ITEM"
 export const TOKEN_CHECK_SUCCESS = "TOKEN_CHECK_SUCCESS";
 export const TOKEN_CHECK_FAILURE = "TOKEN_CHECK_FAILURE";
 
