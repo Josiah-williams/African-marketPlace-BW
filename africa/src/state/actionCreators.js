97b207a1929505a0c2fe287d95ed5638c9ab8982
josiah <josiahdamiwilliams@gmail.com>
2020-03-06 09:20:21 +0100

    action creators

--- africa/src/state/actionCreators.js
+++ africa/src/state/actionCreators.js
@@ -1,15 +1,44 @@
 import * as types from "./actionTypes";
-import axios from "axios";
+import axios from "./auth/axiosWithAuth";
 
+
+//creates an action object with type and payload
+const action = (type, payload) => ({ type, payload });
+//login
+export const login = (credentials, history) => async dispatch => {
+	try {
+	  const response = await axios().post("https://lbs-african-marketplace.herokuapp.com/auth/login", credentials);
+	  const { token, user } = response.data;
+	  localStorage.setItem("token", token);
+	  dispatch(
+		action(types.LOGIN_SUCCESS, {
+		  user,
+		  token,
+		  account_details: user.account_details
+		})
+	  );
+	  history.push(`/items`);
+	} catch (error) {
+	  console.debug(error);
+	}
+  };
+  
+  //logoout
+export const logout = history => dispatch => {
+	localStorage.removeItem("token");
+	dispatch(action(types.LOG_OUT));
+	history.push("/login");
+  };
+  
 // Step 7: Design action creator functions
-export const loadExperiences = () => dispatch => {
-	axios
-		.get("http://localhost:3333/list")
+export const getItems = () => dispatch => {
+	axios()
+		.get("https://lbs-african-marketplace.herokuapp.com/items")
 		.then(response => {
 			dispatch({
-				type: types.GET_list,
+				type: types.GET_ITEMS,
 				payload: {
-					list: response.data
+					list: response.data.list
 				}
 			});
 		})
@@ -18,13 +47,52 @@ export const loadExperiences = () => dispatch => {
 		});
 };
 
-// Step 7: Design action creator functions
-export const updateForm = ({ name, value }) => {
-	return {
-		type: types.UPDATE_FORM,
-		payload: {
-			name,
-			value
-		}
+export const postItems = additems => dispatch => {
+ 
+    dispatch({ type: types.CREATE_ITEMS })
+    axios()
+    .post("https://lbs-african-marketplace.herokuapp.com/item/additems", additems)
+    .then(response => {
+      console.log(response.data);
+      
+      dispatch({
+        type: types.CREATE_ITEMS,
+        payload: response.data
+      });
+    })
+    .catch(() => {
+      console.log("error");
+      
+	});
 	};
+
+	
+	
+export const deleteItem = id =>  dispatch => {
+	console.log(id);
+  
+	const response = axios().delete(`https://lbs-african-marketplace.herokuapp.com/items/${id}`);
+	const deleted = response.data;
+	dispatch(action(types.DELETE_ITEM, id));
+  };
+
+  export const getitems = id => dispatch => {
+	console.log(id);
+  
+	const response = axios().get('https://lbs-african-marketplace.herokuapp.com/items/${id}')
+	const get = response.data;
+	dispatch(action(types.GET_ITEMS, id));
 };
+
+
+export const editItem = updateItem => dispatch => {
+	axios().put('https://lbs-african-marketplace.herokuapp.com/items/${id}`,edititem')
+	.then(response => {
+	   console.log(response.data);
+   
+	dispatch(action(types.EDIT_ITEM, updateItem));
+	})
+	  .catch(error => {
+	   console.log(error);
+   })
+}
