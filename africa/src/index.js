ed7ecd72f40f625f1fef8231282d69a3e2aed5fe
josiah <josiahdamiwilliams@gmail.com>
2020-03-06 09:16:54 +0100

    redux complete

--- africa/src/index.js
+++ africa/src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import thunk from "redux-thunk";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import {
+  appStateReducer,
+  userReducer,
+  tokenReducer,
 
-ReactDOM.render(<App />, document.getElementById('root'));
+} from "./state/reducers";
 
+//Combining all the reducers
+const mainReducer = combineReducers({
+    appState: appStateReducer,
+    user: userReducer,
+    token: tokenReducer,
+});
+  
+//Creating a store
+const store = createStore(
+    mainReducer,
+    {},
+    compose(
+      applyMiddleware(thunk /* ,etc , other middlewares */),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById("root")
+  );
+    
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
