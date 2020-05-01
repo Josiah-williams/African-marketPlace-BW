2149b698eb9d8d3e51c802098477c6925a65e87c
josiah <josiahdamiwilliams@gmail.com>
2020-05-01 17:22:53 +0100

    latest changes using context api

--- africa/src/components/items.js
+++ africa/src/components/items.js
@@ -1,77 +1,131 @@
-import React, { useState, useEffect} from "react";
-import { connect } from "react-redux";
-import  * as actionCreators  from "./state/actionCreators";
-import { useHistory } from "react-router-dom";
+import React, { useContext ,useEffect, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { userContext } from "../context/userContext";
+import { useHistory } from 'react-router-dom'
+import Itemslist from "./itemsList"
 
+export default function Items() {
+  const items = useContext(userContext);
+  const [deleteItem, updateItem,] = useState(items)
+  const [itemslist, getitems] = useState([])
 
+  // const { items } = userContext
 
-export function Item({ getItems, items }) {
-  const history = useHistory();
   useEffect(() => {
-    getItems();
-  }, []);
-  if (!items){return null}
-  
+    getitems()
+  }, [])
+  console.log(items)
+  if (!items) { return null }
   
 
-    
-    const Logout = e => {
-      localStorage.removeItem("token");
-      history.push("/login");
-      
-    };
-    const sell = e => {
-      history.push("/sellerform")
-    }
-      
-    return (
-      <>
-      
-        <h1>items List</h1>
-        {
-        items.map(item => (
-          <div key = {item.id}>
-              
-              <p>id: {item.id}</p>
-              <div className="item-name">
-                <strong>
+
+  
+// const history = useHistory;
+
+  // return <div>'loading'</div>;
+  // console.log(context)
+
+  // if (items.length == 0) {
+ 
+
+  // console.log(context.items)
+
+  return (
+    <div>
+    <itemslist />
+      {items.map(item => {
+        return (
+
+          <div key={item.id}>
+            <h1>items List</h1>
+
+
+
+            <p>id: {item.id}</p>
+            <div className="item-name">
+              <strong>
                 name: {item.name}</strong>
-                </div>
-                <div className="item-description">
-                <strong>description: {item.description}</strong>
-                </div>
-                <div className="item-price">
-                  <strong>price: {item.price}</strong>
-                  </div>
-                  <div className="item-location">
-				           <strong>location: {item.location}</strong>
-                   </div>
-                   <div className="item-category">
-				            <strong>category: {item.category}</strong>
+            </div>
+            <div className="item-description">
+              <strong>description: {item.description}</strong>
+            </div>
+            <div className="item-price">
+              <strong>price: {item.price}</strong>
+            </div>
+            <div className="item-location">
+              <strong>location: {item.location}</strong>
+            </div>
+            <div className="item-category">
+              <strong>category: {item.category}</strong>
+              <div>
+                <button onClick={() => updateItem(item)}>
+                  Update
+              </button>
               </div>
-              
-              
-              </div>
-            
-
-              
-    
-        ))}
-
-      
-        <button onClick={Logout}>Log out</button>
-        <button onClick={sell}>sell</button>
-  </>
-    )
+            </div>
+            <div>
+              {" "}
+              <button onClick={() => deleteItem(item)}>
+                Delete
+              </button>
+            </div>
+            <div>
+              <Form>
+                <label htmlFor="itemForm_name">Name</label>
+                <Field
+                  type="text"
+                  id="itemForm_name"
+                  name="name"
+                  placeholder="Enter item name"
+                  onChange={onchange}
+                  value={item.name}
+                />
+                <label htmlFor="itemForm__description">Description</label>
+                <Field
+                  type="text"
+                  id="itemForm_description"
+                  name="description"
+                  placeholder="Enter item description"
+                  onChange={onchange}
+                  value={item.description}
+                />
+                <label htmlFor="itemForm__price">Price</label>
+                <Field
+                  type="text"
+                  id="itemForm_price"
+                  name="price"
+                  placeholder="Enter item price"
+                  onChange={onchange}
+                  value={item.price}
+                />
+                <label htmlFor="itemForm__location">Location</label>
+                <Field
+                  type="text"
+                  id="itemForm__location"
+                  name="location"
+                  placeholder="Enter item location"
+                  onChange={onchange}
+                  value={item.location}
+                />
+                <label htmlFor="itemForm__category">Category</label>
+                <Field
+                  type="text"
+                  id="itemForm__category"
+                  name="category"
+                  placeholder="Enter item category"
+                />
+                <input type='submit' />
+                <button onClick={oncancel}>Cancel </button>
+              </Form>
+            </div>
+              ) : (
+            <div></div>
+              )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
-   
-   export default connect(
-    function mapStateToProps(state) {
-      console.log(state);
-      
-      return {
-        items: state.items.items
-      };
-    },
-    actionCreators
-  )(Item);
+
+
